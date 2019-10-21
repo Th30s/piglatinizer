e55a620e3b96131da6c7607ec7f2e68b6bb5bac8
Th30s <yahir.garcia@uasdc.org>
2019-10-21 22:06:52 +0000

    10/21/2019. Added pig image

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 $( "document" ).ready(function() { //this helps your jQuery to work. Write all of your JS code in this function.
+    let word = $(".input").val();
+
+    function firstCharacter(string){
+    let suffix = string.substring(0,1);
+    return suffix + "ay";
+    }
+
+    function everthingElse (string){
+    let letters= string.length
+    let suffix = string.substring(1,letters);
+    return suffix;
+    }
 
 
 
-  
-  
-  
-  
+
+
+
 });
+console.log("what");
